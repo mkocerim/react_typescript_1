429da5bb8336068410a57773d794b18e05dad636
Mustafa Koçer <mkocerim@gmail.com>
2023-01-08 22:02:22 +0300

    todo-modal modified

--- src/pages/todo-page/components/todo-modal/todo_modal.tsx
+++ src/pages/todo-page/components/todo-modal/todo_modal.tsx
@@ -1,70 +1,128 @@
+import { FormEvent, useRef, useState } from "react";
+import { TodoType } from "../../todo_page";
+
 export type TodoModalPropsType = {
   show: boolean;
-  onClose:()=>void
+  onClose: () => void;
 };
 
 function TodoModal(props: TodoModalPropsType) {
-    console.log('>>TODO MODAL PROPS',props)
-    console.log('>>TODO MODAL RENDER')
+  console.log(">>TODO MODAL PROPS", props);
+  console.log(">>TODO MODAL RENDER");
 
-  const onClose=()=>{
+  const [inputId, setInputId] = useState<number>(0);
+  const inputTodoRef = useRef<HTMLTextAreaElement>(null);
+  const isDoneRef = useRef<HTMLInputElement>(null);
+  // inputTodoRef.current
+
+  const onClose = () => {
     //modal kapanirken yapilacaklar burada !!
-    console.log('>>>>>TODO MODAL ONCLOSE()')
+    console.log(">>>>>TODO MODAL ONCLOSE()");
 
-    props.onClose()
+    props.onClose();
+  };
 
-  }
-  
-  
-  
-  
-    return (
-    <>
-    {props.show
-    ? (<div className="modal-backdrop fade show"></div>)
-    :(<></>)
+  const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if((inputTodoRef.current === null) || (isDoneRef.current === null)) {
+      return
     }
-    <div
-    // let dizi = ['foo','bar','baz']
-    //dizi.join('-')--> "foo-bar-baz"
-    //dizi.join(' ')--> "foo bar baz"
-   // ["modal","fade","show"].join('') --> "modal fade show"
 
-      onClick={()=>props.onClose()} 
-      className={["modal","fade", props.show ? "show" : ''].join(' ')} style={{display:props.show? 'block' : 'none'}}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">
-              Modal title
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">Modal Content Here</div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary mx-2"
-              onClick={()=>onClose()}
-              
->
-              Close
-            </button>
+    const inputFormData: TodoType={
 
-            <button 
-            type="button" 
-            className="btn btn-primary">
-              Save
-            </button>
+      id:inputId,
+      title:inputTodoRef.current.value,
+      isDone:isDoneRef.current.checked
+    
+    }
+
+    console.log('>>>INPUT ID',inputId)
+    console.log('>>>INPUT TODO REF',inputTodoRef.current.value)
+    console.log('ISDONE REF CURRENT CHECKED',isDoneRef.current.checked)
+    console.log('ISDONE REF CURRENT VALUE',isDoneRef.current.value)
+
+  };
+
+  return (
+    <>
+      {props.show ? <div className="modal-backdrop fade show"></div> : <></>}
+
+      <form onSubmit={onFormSubmit}>
+        <div
+          // let dizi = ['foo','bar','baz']
+          //dizi.join('-')--> "foo-bar-baz"
+          //dizi.join(' ')--> "foo bar baz"
+          // ["modal","fade","show"].join('') --> "modal fade show"
+
+          className={["modal", "fade", props.show ? "show" : ""].join(" ")}
+          style={{ display: props.show ? "block" : "none" }}
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Todo From
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => onClose()}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <div className="mb-3">
+                  <label className="form-label">ID</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="ENTER ID"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                      console.log(typeof event.target.value);
+                      console.log(typeof parseInt(event.target.value));
+
+                      setInputId(parseInt(event.target.value));
+                    }}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Todo Content</label>
+                  <textarea
+                    className="form-control"
+                    ref={inputTodoRef}
+                    onChange={(
+                      event: React.ChangeEvent<HTMLTextAreaElement>
+                    ) => {
+                      console.log(
+                        "TEXTAREA EVENT TARGET VALUE",
+                        event.target.value
+                      );
+                    }}
+                  ></textarea>
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Is Done</label>
+                  <br />
+                  <input ref={isDoneRef} type={"checkbox"} />
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary mx-2"
+                  onClick={() => onClose()}
+                >
+                  Close
+                </button>
+
+                <button type="submit" className="btn btn-primary">
+                  Save
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </form>
     </>
   );
 }
